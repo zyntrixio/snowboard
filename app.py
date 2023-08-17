14fcea09e0cc427b8b9a23259b6a3260575df966
Chris Mitchell <cmitchell@bink.com>
2023-08-17 11:35:38 +0100

    dev deps and poetry fix

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import pandas as pd
 app = dash.Dash(__name__)
 
 cnn = snowflake.connector.connect(
-    user='DWBINKADMIN',
-    password='vXC9W8rvtKm73eLur9kKHc3uFA8L7KcWWrkzrfKVTwsWaD8pfmyPea',
-    account='ci34413.eu-west-2.aws',
-    warehouse='ANALYTICS',
-    role = 'ACCOUNTADMIN'
+    user="DWBINKADMIN",
+    password="vXC9W8rvtKm73eLur9kKHc3uFA8L7KcWWrkzrfKVTwsWaD8pfmyPea",
+    account="ci34413.eu-west-2.aws",
+    warehouse="ANALYTICS",
+    role="ACCOUNTADMIN",
 )
 
 cs = cnn.cursor()
-sql = 'SELECT transaction_id FROM prod.bink_secure.fact_transaction WHERE duplicate_transaction = TRUE'
+sql = "SELECT transaction_id FROM prod.bink_secure.fact_transaction WHERE duplicate_transaction = TRUE"
 cs.execute(sql)
 df = cs.fetch_pandas_all
 print(df)
@@ -29,7 +29,7 @@ cnn.close()
 # Sample data is provided just for demonstration purposes
 
 # Sample data for graphs
-categories = ['Loyalty Cards', 'Payment Cards', 'Users', 'Transactions']
+categories = ["Loyalty Cards", "Payment Cards", "Users", "Transactions"]
 
 overview_graphs = []
 detailed_graphs = []
@@ -39,85 +39,107 @@ for category in categories:
     overview_graphs.append(
         dcc.Graph(
             id=f'{category.lower().replace(" ", "_")}_overview_graph',
-            figure=px.line(x=[1, 2, 3], y=[4, 2, 3], title=f'Overview Graph for {category}')
+            figure=px.line(
+                x=[1, 2, 3], y=[4, 2, 3], title=f"Overview Graph for {category}"
+            ),
         )
     )
-    
+
     detailed_graphs.append(
         dcc.Graph(
             id=f'{category.lower().replace(" ", "_")}_detailed_graph_1',
-            figure=px.bar(x=[1, 2, 3], y=[2, 3, 4], title=f'Detailed Graph 1 for {category}')
+            figure=px.bar(
+                x=[1, 2, 3], y=[2, 3, 4], title=f"Detailed Graph 1 for {category}"
+            ),
         )
     )
     detailed_graphs.append(
         dcc.Graph(
             id=f'{category.lower().replace(" ", "_")}_detailed_graph_2',
-            figure=px.scatter(x=[1, 2, 3], y=[3, 4, 2], title=f'Detailed Graph 2 for {category}')
+            figure=px.scatter(
+                x=[1, 2, 3], y=[3, 4, 2], title=f"Detailed Graph 2 for {category}"
+            ),
         )
     )
     detailed_graphs.append(
         dcc.Graph(
             id=f'{category.lower().replace(" ", "_")}_exploration',
-            figure=px.line(x=[1, 2, 3], y=[2, 2, 2], title=f'Detailed Graph 3 for {category}')
+            figure=px.line(
+                x=[1, 2, 3], y=[2, 2, 2], title=f"Detailed Graph 3 for {category}"
+            ),
         )
     )
 
 # App layout
-app.layout = html.Div([
-    dcc.Tabs(id='tabs', value='overview', children=[
-        dcc.Tab(label='Overview', value='overview'),
-        dcc.Tab(label='Loyalty Cards', value='loyalty_cards'),
-        dcc.Tab(label='Payment Cards', value='payment_cards'),
-        dcc.Tab(label='Users', value='users'),
-        dcc.Tab(label='Transactions', value='transactions'),
-    ]),
-    html.Div(id='page-content')
-])
+app.layout = html.Div(
+    [
+        dcc.Tabs(
+            id="tabs",
+            value="overview",
+            children=[
+                dcc.Tab(label="Overview", value="overview"),
+                dcc.Tab(label="Loyalty Cards", value="loyalty_cards"),
+                dcc.Tab(label="Payment Cards", value="payment_cards"),
+                dcc.Tab(label="Users", value="users"),
+                dcc.Tab(label="Transactions", value="transactions"),
+            ],
+        ),
+        html.Div(id="page-content"),
+    ]
+)
+
 
 # Callback to update page content based on selected tab
-@app.callback(Output('page-content', 'children'), [Input('tabs', 'value')])
+@app.callback(Output("page-content", "children"), [Input("tabs", "value")])
 def display_content(selected_tab):
-    if selected_tab == 'overview':
-        return html.Div([
-            html.H1('High-Level Overview'),
-            html.Div([
-                html.H2('Loyalty Cards'),
-                *overview_graphs[:3]
-            ]),
-            html.Div([
-                html.H2('Payment Cards'),
-                *overview_graphs[3:6]
-            ]),
-            html.Div([
-                html.H2('Users'),
-                *overview_graphs[6:9]
-            ]),
-            html.Div([
-                html.H2('Transactions'),
-                *overview_graphs[9:12]
-            ])
-        ])
+    if selected_tab == "overview":
+        return html.Div(
+            [
+                html.H1("High-Level Overview"),
+                html.Div([html.H2("Loyalty Cards"), *overview_graphs[:3]]),
+                html.Div([html.H2("Payment Cards"), *overview_graphs[3:6]]),
+                html.Div([html.H2("Users"), *overview_graphs[6:9]]),
+                html.Div([html.H2("Transactions"), *overview_graphs[9:12]]),
+            ]
+        )
     else:
         category_index = categories.index(selected_tab.capitalize())
-        return html.Div([
-            html.H1(f'{selected_tab} Details'),
-            html.Div([
-                html.H2('Section 1'),
-                *detailed_graphs[category_index * 12: category_index * 12 + 3]
-            ]),
-            html.Div([
-                html.H2('Section 2'),
-                *detailed_graphs[category_index * 12 + 3: category_index * 12 + 6]
-            ]),
-            html.Div([
-                html.H2('Section 3'),
-                *detailed_graphs[category_index * 12 + 6: category_index * 12 + 9]
-            ]),
-            html.Div([
-                html.H2('Section 4'),
-                *detailed_graphs[category_index * 12 + 9: category_index * 12 + 12]
-            ])
-        ])
+        return html.Div(
+            [
+                html.H1(f"{selected_tab} Details"),
+                html.Div(
+                    [
+                        html.H2("Section 1"),
+                        *detailed_graphs[category_index * 12 : category_index * 12 + 3],
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.H2("Section 2"),
+                        *detailed_graphs[
+                            category_index * 12 + 3 : category_index * 12 + 6
+                        ],
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.H2("Section 3"),
+                        *detailed_graphs[
+                            category_index * 12 + 6 : category_index * 12 + 9
+                        ],
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.H2("Section 4"),
+                        *detailed_graphs[
+                            category_index * 12 + 9 : category_index * 12 + 12
+                        ],
+                    ]
+                ),
+            ]
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
