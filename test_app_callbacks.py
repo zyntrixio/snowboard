5b21f44e6dad089471e1900fe18c290a4d5a25b4
Chris Mitchell <cmitchell@bink.com>
2023-12-12 21:08:57 +0000

    lint and basic test

--- test_app_callbacks.py
+++ test_app_callbacks.py
@@ -5,15 +5,11 @@ from dash._utils import AttributeDict
 # Import the names of callback functions you want to test
 from app import display_content
 
-def test_update_callback():
-    output = update(1, 0)
-    assert output == 'button 1: 1 & button 2: 0'
-
 def test_display_callback():
     def run_callback():
-        context_value.set(AttributeDict(**{"triggered_inputs": [{"prop_id": "btn-1-ctx-example.n_clicks"}]}))
-        return display_content(1, 0, 0)
+        context_value.set(AttributeDict(**{"triggered_inputs": [{"prop_id": "Section 1"}]}))  # noqa: E501
+        return display_content('overview')
 
     ctx = copy_context()
     output = ctx.run(run_callback)
-    assert output == f'You last clicked button with ID btn-1-ctx-example'
+    assert output == 'You last clicked button with ID btn-1-ctx-example'
