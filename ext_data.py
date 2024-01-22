8dec1312459a57c184ee422a6fe0efe9007064cd
Chris Mitchell <cmitchell@bink.com>
2024-01-22 19:00:37 +0000

    docs 2

--- ext_data.py
+++ ext_data.py
@@ -3,9 +3,8 @@ from typing import Optional
 from env import cnn
 
 def fetch_data_from_snowflake(sql_command: str) -> Optional[pd.DataFrame]:
-    """
-    Executes a SQL command on a Snowflake data warehouse
-      and returns the results as a Pandas DataFrame.
+    """Executes a SQL command on a Snowflake data warehouse
+       and returns the results as a Pandas DataFrame.
 
     :param sql_command: SQL query to execute
     :return: Pandas DataFrame containing the query results or None if an error occurs
@@ -22,3 +21,4 @@ def fetch_data_from_snowflake(sql_command: str) -> Optional[pd.DataFrame]:
         print(f"An error occured: {e}")
     finally:
         cnn.close()
+
